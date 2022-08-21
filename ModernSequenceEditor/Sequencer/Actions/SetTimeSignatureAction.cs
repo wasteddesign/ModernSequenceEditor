9dfed25458c354bdbbcfc63f9651ff438e9bd591
wasteddesign <111274792+wasteddesign@users.noreply.github.com>
2022-08-21 12:25:35 +0300

    Add files via upload

---
+++ ModernSequenceEditor/Sequencer/Actions/SetTimeSignatureAction.cs
@@ -0,0 +1,32 @@
+﻿using BuzzGUI.Interfaces;
+
+namespace WDE.ModernSequenceEditor.Actions
+{
+    class SetTimeSignatureAction : IAction
+	{
+		TimeSignatureList tsl;
+		TimeSignatureList oldtsl;
+
+		int time;
+		int step;
+
+		public SetTimeSignatureAction(TimeSignatureList tsl, int t, int st)
+		{
+			this.tsl = tsl;
+			oldtsl = new TimeSignatureList(tsl);
+			time = t;
+			step = st;
+		}
+
+		public void Do()
+		{
+			tsl.Set(time, step);
+		}
+
+		public void Undo()
+		{
+			tsl.Clone(oldtsl);
+		}
+
+	}
+}
